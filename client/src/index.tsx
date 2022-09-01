581b95e18a5f7790dec27beb9e703bcec7d2a63f
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-08-31 20:35:06 -0700

    fix: no non-null assertion band-aid

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
