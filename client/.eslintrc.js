1b960376603abff93ad1ae8a5a1e155d9e80f533
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-08-31 20:40:19 -0700

    fix: fixed lint errors running in server and not client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,12 +10,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    "tsdoc/syntax": "off",
+    "tsdoc/syntax": "warn",
     // all of these are being suppressed - TODO is to deal with them later
-    "no-unused-vars": ["off", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
     "no-empty-function": "off",
-    "@typescript-eslint/no-empty-function": ["off"],
-    "indent": ["off", 2]
+    "@typescript-eslint/no-empty-function": ["warn"],
+    "indent": ["warn", 2]
   },
   env: {
     node: true
