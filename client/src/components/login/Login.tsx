fc309eb60a87ee22469893719604f1ed0ac19eb9
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-10-21 08:05:13 -0500

    fix: remove waitlist import

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import { waitlistEmails } from "../../lib/waitlistEmails";
 import IvyLottie from "../login/IvyLottie";
 
 const Login: React.FC = (): JSX.Element => {
