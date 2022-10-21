f5acca65a0543c25b5feee2276ea08c02398552f
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-10-21 08:32:00 -0500

    fix: make majors and minors available

--- client/src/components/settings/Minors.tsx
+++ client/src/components/settings/Minors.tsx
@@ -17,14 +17,16 @@ export const Minors: React.FC<MinorsProps> = ({
 }): JSX.Element => {
   return (
     <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-      <dt className="text-sm font-medium text-gray-500">Minors (disabled for pre-release)</dt>
+      <dt className="text-sm font-medium text-gray-500">
+        Minor (only one minor supported at this time)
+      </dt>
       <dd>
         <input
           ref={minorRef}
           type="search"
           name="minors"
           id="minors"
-          className="pointer-events-none select-none block w-[29.5rem] rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+          className="block w-[29.5rem] rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
           placeholder="Not specified"
           value={minor ? minor.minorTitle : ""}
           onChange={() => {
